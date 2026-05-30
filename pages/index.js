47ad7ae734f524e72be9389bcde75e695d1b28ce
Your Name <you@example.com>
2026-05-29 20:00:37 -0400

    Temporarily redirect homepage to leaderboard

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+// pages/index.js
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+export default function HomeRedirect() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace("/leaderboard");
+  }, [router]);
+
+  return null;
+}
